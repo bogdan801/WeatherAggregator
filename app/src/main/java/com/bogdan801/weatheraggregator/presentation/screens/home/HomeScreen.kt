355eca9f2f40194e7addc21c9ef62534307d919a
bogdan801 <bogdan2002801@gmail.com>
2022-12-23 23:01:29 +0200

    Finished work on animated transition between themes

--- app/src/main/java/com/bogdan801/weatheraggregator/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/bogdan801/weatheraggregator/presentation/screens/home/HomeScreen.kt
@@ -1,7 +1,8 @@
 package com.bogdan801.weatheraggregator.presentation.screens.home
 
 import android.graphics.Bitmap
-import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -26,6 +27,7 @@ import androidx.navigation.NavHostController
 import com.bogdan801.weatheraggregator.R
 import com.bogdan801.weatheraggregator.presentation.theme.Theme
 import com.bogdan801.weatheraggregator.presentation.theme.WeatherAggregatorTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(
@@ -34,6 +36,7 @@ fun HomeScreen(
 ){
     val context = LocalContext.current
     val view = LocalView.current
+    val coroutineScope = rememberCoroutineScope()
 
     val scaffoldState = rememberScaffoldState()
     Scaffold(
@@ -41,10 +44,11 @@ fun HomeScreen(
         scaffoldState = scaffoldState,
         snackbarHost = {scaffoldState.snackbarHostState}
     ) {
+        //states for animated theme transition
         val imageState = remember { mutableStateOf<ImageBitmap?>(null) }
         val iconCoordinates = remember { mutableStateOf(Offset.Zero) }
-
         val isAnimating = remember { mutableStateOf(false)}
+        val animatable = remember { Animatable(50f) }
 
         Column(modifier = Modifier
             .fillMaxSize()
@@ -86,27 +90,38 @@ fun HomeScreen(
                 }
 
                 var count by remember { mutableStateOf(viewModel.themeState.value.ordinal) }
-                val isSystemDark = isSystemInDarkTheme()
+                var isEnabled by remember { mutableStateOf(true) }
                 IconButton(
                     modifier = Modifier
                         .padding(end = 8.dp)
                         .onGloballyPositioned { coordinates ->
                             iconCoordinates.value = coordinates.boundsInRoot().center
                         },
+                    enabled = isEnabled,
                     onClick = {
                         count++
                         viewModel.setTheme(count % 3, context)
 
-                        if(count % 3 == 2 || (count % 3 == 0 && isSystemDark))
-                        isAnimating.value = true
+                        //animated transition between themes
+                        coroutineScope.launch {
+                            isEnabled = false
 
+                            //save screenshot
+                            val bmp = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888).applyCanvas {
+                                view.draw(this)
+                            }
+                            imageState.value = bmp.asImageBitmap()
 
-                        val bmp = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888).applyCanvas {
-                            view.draw(this)
+                            isAnimating.value = true
+                            animatable.animateTo(
+                                2500f,
+                                tween(350)
+                            )
+                            isAnimating.value = false
+
+                            animatable.snapTo(50f)
+                            isEnabled = true
                         }
-                        imageState.value = bmp.asImageBitmap()
-
-
                     },
                 ) {
                     Icon(
@@ -122,27 +137,31 @@ fun HomeScreen(
                     )
                 }
             }
-
         }
 
-        val radiusState by animateFloatAsState(targetValue = if (isAnimating.value) 4000f else 0f)
+        /////CONTENT//////
 
 
-        Box(modifier = Modifier
-            .fillMaxSize()
-            //.graphicsLayer(alpha = 0.99f)
-            .drawBehind {
-                if (imageState.value != null) {
-                    //drawImage(imageState.value!!)
-                    drawCircle(
-                        color = Color.Black,
-                        radius = radiusState,
-                        center = iconCoordinates.value,
-                        blendMode = BlendMode.Xor
-                    )
+        /////////////////
+
+        //canvas that displays an animated theme transition
+        if (isAnimating.value){
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .graphicsLayer(alpha = 0.99f)
+                .drawBehind {
+                    if (imageState.value != null) {
+                        drawImage(imageState.value!!)
+                        drawCircle(
+                            color = Color.Black,
+                            radius = animatable.value,
+                            center = iconCoordinates.value,
+                            blendMode = BlendMode.Xor
+                        )
+                    }
                 }
-            }
-        )
+            )
+        }
     }
 }
 
@@ -150,51 +169,6 @@ fun HomeScreen(
 @Preview
 fun PreviewMain() {
     WeatherAggregatorTheme(Theme.Light) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(MaterialTheme.colors.secondary),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Button(
-                modifier = Modifier.padding(8.dp),
-                onClick = { /*TODO*/ },
-                shape = MaterialTheme.shapes.large,
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = MaterialTheme.colors.secondary,
-                    contentColor = MaterialTheme.colors.onSurface
-                ),
-                elevation = null,
-                border = BorderStroke(width = 1.dp, color = Color.White.copy(alpha = 0.5f))
-            ) {
-                Row {
-                    Icon(painter = painterResource(id = R.drawable.ic_location), contentDescription = "location")
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(
-                        text = "Change location",
-                        style = MaterialTheme.typography.h4,
-                        //color = MaterialTheme.colors.onSurface
-                    )
-                }
-            }
 
-            //var count by remember { mutableStateOf(viewModel.themeState.value.ordinal) }
-            IconButton(
-                modifier = Modifier.padding(end = 8.dp),
-                onClick = {
-                    //count++
-                    //viewModel.setTheme(count % 3, context)
-                },
-            ) {
-                Icon(
-                    painter = painterResource(
-                        id = R.drawable.ic_light_mode
-                    ),
-                    contentDescription = "",
-                    tint = MaterialTheme.colors.onSurface
-                )
-            }
-        }
     }
 }
