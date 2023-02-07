168c66a5e09e56b3376cb22ee87628a4e02158a3
bogdan801 <bogdan2002801@gmail.com>
2023-02-07 23:23:22 +0200

    Made adaptive day boxes in DataSourceCard

--- app/src/main/java/com/bogdan801/weatheraggregator/presentation/composables/repeatable/DataSourceCard.kt
+++ app/src/main/java/com/bogdan801/weatheraggregator/presentation/composables/repeatable/DataSourceCard.kt
@@ -1,5 +1,6 @@
 package com.bogdan801.weatheraggregator.presentation.composables.repeatable
 
+import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.core.animateDpAsState
@@ -13,6 +14,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -22,10 +26,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.bogdan801.weatheraggregator.data.util.toFormattedString
 import com.bogdan801.weatheraggregator.presentation.screens.home.WeatherDataState
 
 @Composable
@@ -36,6 +41,7 @@ fun DataSourceCard(
     onLongPress: (Boolean) -> Unit = {},
     onTap: (Boolean) -> Unit ={}
 ) {
+    val density = LocalDensity.current
     var isExpanded by remember { mutableStateOf(false) }
     Box(modifier = modifier){
         val cardHeight by animateDpAsState(
@@ -121,9 +127,7 @@ fun DataSourceCard(
                     visible = isExpanded,
                     enter = EnterTransition.None
                 ) {
-                    Row(modifier = Modifier
-                        .fillMaxSize()
-                    ) {
+                    Row(modifier = Modifier.fillMaxSize()) {
                         var dayLineHeight by remember { mutableStateOf(0.dp) }
                         ExpandableWeatherSlice(
                             modifier = Modifier
@@ -138,38 +142,102 @@ fun DataSourceCard(
                                 dayLineHeight = height
                             }
                         )
-                        
-                        Column(
-                            modifier = Modifier
-                                .fillMaxHeight()
-                                .weight(1f)
-                        ) {
-                            Box(modifier = Modifier
-                                .fillMaxWidth()
-                                .height(dayLineHeight)
-                                .background(MaterialTheme.colors.surface)
-                            )
+
+
+                        BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+                            val width = maxWidth
+                            val sliceWidth = 70.dp
+                            val lazyListState = rememberLazyListState()
+
+                            val firstVisibleIndex by remember {
+                                derivedStateOf {
+                                    val visibleList = lazyListState.layoutInfo.visibleItemsInfo
+                                    if(visibleList.isNotEmpty()) visibleList[0].index
+                                    else null
+                                }
+                            }
+                            val lastVisibleIndex by remember {
+                                derivedStateOf {
+                                    val visibleList = lazyListState.layoutInfo.visibleItemsInfo
+                                    if(visibleList.isNotEmpty()) visibleList.last().index
+                                    else null
+                                }
+                            }
+                            val firstVisibleOffset by remember {
+                                derivedStateOf {
+                                    with(density) {
+                                        lazyListState.firstVisibleItemScrollOffset.toDp()
+                                    }
+                                }
+                            }
+                            val lastVisibleOffset by remember {
+                                derivedStateOf {
+                                    if((firstVisibleIndex != null) && (lastVisibleIndex != null)){
+                                        var sumWidth = 0.dp
+                                        (firstVisibleIndex!!..lastVisibleIndex!!).forEach { id ->
+                                            sumWidth += sliceWidth * dataState.data.weatherByDates[id].weatherByHours.size
+                                        }
+
+                                        val result = sumWidth - (firstVisibleOffset + width)
+
+                                        return@derivedStateOf if(result >= 0.dp) result else 0.dp
+                                    }
+                                    else 0.dp
+                                }
+                            }
+
                             LazyRow(
                                 modifier = Modifier
-                                    .fillMaxWidth()
-                                    .weight(1f)
+                                    .fillMaxSize(),
+                                state = lazyListState
                             ){
-                                dataState.data.weatherByDates[0].weatherByHours.forEachIndexed { index, weatherSlice ->
-                                    item {
-                                        ExpandableWeatherSlice(
+                                itemsIndexed(dataState.data.weatherByDates) { index, day ->
+                                    val itemWidth = sliceWidth * day.weatherByHours.size
+
+                                    val startOffset =
+                                        if((firstVisibleIndex != null) && (firstVisibleIndex == index)) firstVisibleOffset
+                                        else 0.dp
+
+                                    val lastOffset =
+                                        if((lastVisibleIndex != null) && (lastVisibleIndex == index)) lastVisibleOffset
+                                        else 0.dp
+
+                                    Column(modifier = Modifier.fillMaxHeight()) {
+                                        Box(
                                             modifier = Modifier
-                                                .width(70.dp)
-                                                .fillMaxHeight()
-                                                .background(
-                                                    if (index % 2 == 0) MaterialTheme.colors.surface.copy(
-                                                        0.42f
-                                                    )
-                                                    else MaterialTheme.colors.onPrimary
+                                                .width(itemWidth)
+                                                .height(dayLineHeight)
+                                                .padding(
+                                                    start = startOffset,
+                                                    end = lastOffset + 0.5.dp
                                                 )
-                                                .padding(bottom = 4.dp),
-                                            isExpanded = true,
-                                            data = weatherSlice
-                                        )
+                                                .background(MaterialTheme.colors.surface),
+                                            contentAlignment = Alignment.Center
+                                        ){
+                                            Text(
+                                                text = day.date.toFormattedString(),
+                                                color = MaterialTheme.colors.onSurface.copy(0.8f),
+                                                style = MaterialTheme.typography.caption,
+                                                maxLines = 1,
+                                                overflow = TextOverflow.Ellipsis
+                                            )
+                                        }
+                                        Row(modifier = Modifier.fillMaxHeight()) {
+                                            day.weatherByHours.forEachIndexed { index, weatherSlice ->
+                                                ExpandableWeatherSlice(
+                                                    modifier = Modifier
+                                                        .width(sliceWidth)
+                                                        .fillMaxHeight()
+                                                        .background(
+                                                            if (index % 2 == 0) MaterialTheme.colors.surface.copy(0.42f)
+                                                            else MaterialTheme.colors.onPrimary
+                                                        )
+                                                        .padding(bottom = 4.dp),
+                                                    isExpanded = true,
+                                                    data = weatherSlice
+                                                )
+                                            }
+                                        }
                                     }
                                 }
                             }
