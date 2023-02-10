1e283503e6ce52aa632993754dce57241155e941
bogdan801 <bogdan2002801@gmail.com>
2023-02-10 23:03:00 +0200

    Fixed isEven bug in DataSourceCard

--- app/src/main/java/com/bogdan801/weatheraggregator/presentation/composables/repeatable/DataSourceCard.kt
+++ app/src/main/java/com/bogdan801/weatheraggregator/presentation/composables/repeatable/DataSourceCard.kt
@@ -1,6 +1,5 @@
 package com.bogdan801.weatheraggregator.presentation.composables.repeatable
 
-import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.core.animateDpAsState
@@ -14,7 +13,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Card
@@ -31,6 +29,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.bogdan801.weatheraggregator.data.util.toFormattedString
+import com.bogdan801.weatheraggregator.domain.model.WeatherData
 import com.bogdan801.weatheraggregator.presentation.screens.home.WeatherDataState
 
 @Composable
@@ -122,7 +121,6 @@ fun DataSourceCard(
                     }
                 }
 
-
                 AnimatedVisibility(
                     visible = isExpanded,
                     enter = EnterTransition.None
@@ -142,7 +140,6 @@ fun DataSourceCard(
                                 dayLineHeight = height
                             }
                         )
-
 
                         BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
                             val width = maxWidth
@@ -191,15 +188,15 @@ fun DataSourceCard(
                                     .fillMaxSize(),
                                 state = lazyListState
                             ){
-                                itemsIndexed(dataState.data.weatherByDates) { index, day ->
+                                itemsIndexed(dataState.data.weatherByDates) { dayIndex, day ->
                                     val itemWidth = sliceWidth * day.weatherByHours.size
 
                                     val startOffset =
-                                        if((firstVisibleIndex != null) && (firstVisibleIndex == index)) firstVisibleOffset
+                                        if((firstVisibleIndex != null) && (firstVisibleIndex == dayIndex)) firstVisibleOffset
                                         else 0.dp
 
                                     val lastOffset =
-                                        if((lastVisibleIndex != null) && (lastVisibleIndex == index)) lastVisibleOffset
+                                        if((lastVisibleIndex != null) && (lastVisibleIndex == dayIndex)) lastVisibleOffset
                                         else 0.dp
 
                                     Column(modifier = Modifier.fillMaxHeight()) {
@@ -223,13 +220,13 @@ fun DataSourceCard(
                                             )
                                         }
                                         Row(modifier = Modifier.fillMaxHeight()) {
-                                            day.weatherByHours.forEachIndexed { index, weatherSlice ->
+                                            day.weatherByHours.forEachIndexed { sliceIndex, weatherSlice ->
                                                 ExpandableWeatherSlice(
                                                     modifier = Modifier
                                                         .width(sliceWidth)
                                                         .fillMaxHeight()
                                                         .background(
-                                                            if (index % 2 == 0) MaterialTheme.colors.surface.copy(0.42f)
+                                                            if (isSliceEven(dayIndex, sliceIndex, dataState.data)) MaterialTheme.colors.surface.copy(0.42f)
                                                             else MaterialTheme.colors.onPrimary
                                                         )
                                                         .padding(bottom = 4.dp),
@@ -247,4 +244,14 @@ fun DataSourceCard(
             }
         }
     }
+}
+
+fun isSliceEven(dayIndex: Int, sliceIndex: Int, data: WeatherData): Boolean{
+    var sum = 0
+    (0 until dayIndex).forEach { index ->
+        sum += data.weatherByDates[index].weatherByHours.size
+    }
+    sum += sliceIndex
+
+    return sum % 2 == 0
 }
